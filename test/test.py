8dbefee75893a69943b5119378403dd015df713f
danrong <dl3333@columbia.edu>
2021-01-19 14:38:09 -0500

    testing pycharm

--- test/test.py
+++ test/test.py
@@ -2,8 +2,8 @@ class Car:
 
     def __init__(self, speed=0):
         self.speed = speed
-        self.odometer = 0
-        self.time = 0
+        self.odometer = 10
+        self.time = 10
 
     def say_state(self):
         print("I'm going {} kph!".format(self.speed))
