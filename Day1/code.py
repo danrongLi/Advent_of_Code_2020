d904f407e1de7cb1db16984425809bd78e01a41d
danrong <dl3333@columbia.edu>
2021-01-21 13:46:44 -0500

    Day 1: Report Repair

--- Day1/code.py
+++ Day1/code.py
@@ -1,5 +1,6 @@
 def readFile() -> list:
     with open(f"{__file__.rstrip('code.py')}input.txt", "r") as f:
+        # return f.readlines()
         return [int(line[:-1]) for line in f.readlines()]
 
 def part1(vals: list) -> int:
@@ -14,13 +15,7 @@ def part2(vals: list) -> int:
                 return vals[i] * vals[j] *  (2020 - vals[i] - vals[j])
 
 
-def test():
-    test_input = [1721, 979, 366, 299, 675, 1456]
-    assert part1(test_input) == 514579
-    assert part2(test_input) == 241861950
-
 if __name__ == "__main__":
-    test()
     vals = readFile()
     print(f"Part 1: {part1(vals)}")
     print(f"Part 2: {part2(vals)}")
