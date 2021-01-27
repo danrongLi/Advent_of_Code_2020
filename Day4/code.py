fde4a504beaaa95945ebeb5584daa154fd5eebaa
danrong <dl3333@columbia.edu>
2021-01-27 15:09:33 -0500

    Day 4: Passport Processing

--- Day4/code.py
+++ Day4/code.py
@@ -1,9 +1,12 @@
+import re
+
 def readfile():
     result = list()
     with open("input.txt", "r") as f:
         for lines in f.read()[:-1].split("\n\n"):
             result.append(dict(d.split(":") for d in lines.split()))
     return result
+
 
 def part1(data):
     required_keys = ['eyr', 'hgt', 'pid', 'ecl', 'byr', 'hcl', 'iyr']
@@ -19,10 +22,27 @@ def part1(data):
         if if_valid_keys(passport):
             part1_valid.append(passport)
 
-    return len(part1_valid)
+    return part1_valid
+
+
+def part2(part1_ans):
+
+    pattern = {
+        "byr": "19[2-9][0-9]|200[0-2]",
+        "iyr": "20(1[0-9]|20)",
+        "eyr": "20(2[0-9]|30)",
+        "hgt": "1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in",
+        "hcl": "#[0-9a-f]{6}",
+        "ecl": "amb|blu|brn|gry|grn|hzl|oth",
+        "pid": "[0-9]{9}",
+        "cid": ".*"
+    }
+
+    return sum((all((re.fullmatch(pattern[key], passport[key]) for key in passport)) for passport in part1_ans))
 
 
 if __name__ == "__main__":
     data = readfile()
     part1_ans = part1(data)
-    print(part1_ans)
+    print(len(part1_ans))
+    print(part2(part1_ans))
