622127d6ca41ec81870b6582f4e582c3ab0ac765
danrong <dl3333@columbia.edu>
2021-01-27 12:36:00 -0500

    Day 3: Toboggan Trajectory

--- Day3/code.py
+++ Day3/code.py
@@ -16,7 +16,17 @@ def part1(data, dx, dy):
             tree += 1
     return tree
 
+def part2(data):
+    tree11 = part1(data,1,1)
+    tree31 = part1(data,3,1)
+    tree51 = part1(data,5,1)
+    tree71 = part1(data,7,1)
+    tree12 = part1(data,1,2)
+    return tree11 * tree31 * tree51 * tree71 * tree12
+
 if __name__ == "__main__":
     data = readfile()
     part1_trees = part1(data, 3, 1)
     print(part1_trees)
+    part2_trees = part2(data)
+    print(part2_trees)
